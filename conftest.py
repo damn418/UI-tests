9fcb3d58ce3d37bb6c760889474de57a1376485a
Damn418 <damn543@yandex.com>
2026-03-30 15:42:39 +0300

    ci push

--- conftest.py
+++ conftest.py
@@ -5,10 +5,10 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(autouse=True, scope="function")
 def driver(request):
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
+    # options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     yield driver
